d9f9b9a6f1819bcd19fd961996319a4e16e5f2e3
Hasan <hzafar300@outlook.com>
2022-07-18 16:12:46 -0400

    first

---
+++ allocation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//Github: keyfive5
+//Github: Wrenen
+//Make github public
+//https://github.com/keyfive5/Assignment-4
+
+
+int main(int argc, char **argv) {
+
+
+	//Allocated Memory
+	if (argc == 2) {
+		printf("Allocated 1048576 bytes of memory \n");
+	}
+
+	return 0;
+}
