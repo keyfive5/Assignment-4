a6238ce86069c1bd6361dd6c590f4a12507639c0
Hasan <hzafar300@outlook.com>
2022-07-18 18:55:25 -0400

    Started Status, Release, Request, etc

--- allocation.c
+++ allocation.c
@@ -6,37 +6,69 @@
 //https://github.com/keyfive5/Assignment-4
 
 //functions
+void request();
+void release();
+void status();
 void help_menu();
 void unrecognized();
 
 int main(int argc, char **argv) {
 
-
-	//Allocated Memory
 	if (argc == 2) {
-		printf("Allocated %s bytes of memory \n", argv[1]);
+
+		//Allocate the memory here
+
+		printf("Allocated %s bytes of memory\n", argv[1]);
+
+		char cmd[50];
+		printf("allocator>");
+		scanf("%s",cmd);
+
+		while(strcmp(cmd,"Exit")){
+
+			// Launchs help menu
+			if(!strcmp(cmd,"Help")) {
+			    help_menu();
+
+			//Change Request to fix
+			} else if (!strcmp(cmd,"RQ")) {
+				request();
+			} else if (!strcmp(cmd,"RL")) {
+				release();
+			} else if (!strcmp(cmd,"Status")) {
+				status();
+			} else {
+			    unrecognized();
+			}
+
+			printf("allocator>");
+			scanf("%s",cmd);
+
+			}
+
+			return 0;
+
 	} else {
-		printf("Error\n");
-		return 1;
+		printf("Error");
 	}
-	char cmd[50];
-	printf("allocator>");
-	scanf("%s",cmd);
+}
 
-	while(strcmp(cmd,"Exit")){
+//Request
+//Fix to actually request
+void request() {
+	printf("Successfully allocated __ to process __\n");
+}
 
-	// Launchs help menu
-	if(!strcmp(cmd,"Help"))
-	    help_menu();
+//Release
+//Fix to actually release
+void release() {
+	printf("releasing memory for process __\n");
+	printf("Successfully released memory for process __\n");
+}
 
-	else
-	    unrecognized();
-
-	printf("allocator>");
-	scanf("%s",cmd);
-	}
-
-	return 0;
+//Finish to actually do the status stuff
+void status() {
+	printf("insert status here\n");
 }
 
 // lists functions that the program recognizes
@@ -48,7 +80,7 @@ void help_menu(){
     printf("\t\t\"F\" - First fit algorithm\n");
     printf("\t\t\"B\" - First best algorithm\n");
     printf("\t\t\"W\" - First worst algorithm\n");
-    printf("\t\"RL <process number/name>\" - Realeases the memory of the specified process\n");
+    printf("\t\"RL <process number/name>\" - Releases the memory of the specified process\n");
     printf("\t\"Status\" - Reports the status of memory entered\n");
     printf("\t\"Exit\" - Exits the program\n");
     printf("\n");
